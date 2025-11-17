42b573271f25c4c33727d48e89221979e5e06441
auth-menn <auth.menn@gmail.com>
2025-11-18 02:42:53 +0700

    update

--- resources/js/Pages/Auth/VerifyEmail.tsx
+++ resources/js/Pages/Auth/VerifyEmail.tsx
@@ -1,13 +1,17 @@
 import PrimaryButton from '@/Components/PrimaryButton';
 import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, Link, useForm } from '@inertiajs/react';
+import React from 'react';
 
-export default function VerifyEmail({ status }) {
+interface VerifyEmailProps {
+    status?: string;
+}
+
+export default function VerifyEmail({ status }: VerifyEmailProps) {
     const { post, processing } = useForm({});
 
-    const submit = (e) => {
+    const submit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
         post(route('verification.send'));
     };
 
