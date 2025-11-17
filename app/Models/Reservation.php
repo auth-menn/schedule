517e9a8cadb39a2f2bee80955079d909cc5dcbf3
auth-menn <auth.menn@gmail.com>
2025-11-18 00:21:01 +0700

    update

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -2,19 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Reservation extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'room_id',
         'user_id',
         'title',
-        'description',
         'start_time',
         'end_time',
-        'status', // Tambahkan ini
+        'status',
     ];
 
     protected $casts = [
@@ -22,13 +23,19 @@ class Reservation extends Model
         'end_time' => 'datetime',
     ];
 
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function room(): BelongsTo
+    /**
+     * Relasi ke Room
+     */
+    public function room()
     {
         return $this->belongsTo(Room::class);
+    }
+
+    /**
+     * Relasi ke User
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
