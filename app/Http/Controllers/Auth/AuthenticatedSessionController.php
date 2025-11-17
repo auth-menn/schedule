58c6e99612ca3343955edccd6adbdb151814b95f
auth-menn <auth.menn@gmail.com>
2025-11-17 23:12:24 +0700

    update

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,12 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+       return redirect()->intended(
+        auth()->user()->role === 'admin'
+            ? route('admin.dashboard')
+            : route('rooms.index')
+    );
+
     }
 
     /**
