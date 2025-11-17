3cfa82250518d282d3a4b9e0075fde6d6dead32c
auth-menn <auth.menn@gmail.com>
2025-11-18 02:48:15 +0700

    update

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Room extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'capacity',
+        'location',
+        'facilities',
+        'photo',
+    ];
+
+    public function getPhotoUrlAttribute()
+    {
+        return $this->photo ? asset('storage/' . $this->photo) : null;
+    }
 }
